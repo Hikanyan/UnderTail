54d3a677146eab50ce518c3dee962acfffb7de13
KoiiroHikanyan <86185936+Hikanyan@users.noreply.github.com>
2022-11-21 17:56:58 +0900

    【Update】GameManager

--- Assets/Hikanyan_Assets/Script/GameManager.cs
+++ Assets/Hikanyan_Assets/Script/GameManager.cs
@@ -2,7 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameManager : MonoBehaviour
 {
-    
+    [SerializeField, Tooltip("•”‰®‚Ì”Ô†")] int _loomNum;
+
+    public static GameManager Instance;
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else
+        {
+            DontDestroyOnLoad(this);
+        }
+    }
 }
