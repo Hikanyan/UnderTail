a8e3fc2e63b024dc313f93c5e4d129cc1af29680
Hikanyan <hikanyan0131@gmail.com>
2022-11-24 21:08:47 +0900

    【Update】GameManager

--- Assets/Hikanyan_Assets/Script/GameManager.cs
+++ Assets/Hikanyan_Assets/Script/GameManager.cs
@@ -1,10 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-
-public class GameManager : MonoBehaviour
+public class GameManager : SingletonBehaviour<GameManager>
 {
     [SerializeField, Tooltip("•”‰®‚Ì”Ô†")] int _loomNum;
     [SerializeField] int _loomIntensity;
+
+
+    
+    void ChangeScene()
+    {
+        SceneManager.LoadScene(_loomIntensity);
+    } 
 }
